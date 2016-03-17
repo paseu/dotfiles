b9616be94da5bccc8d9ffb22b359a69e2cfd0e85
rienbar <paseu74@gmail.com>
2016-03-17 23:39:26 +0900

    modified:awesome/rc.lua - moved urxvtd to xprofile

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -466,7 +466,6 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 local xresources_name = "awesome.started"
 local xresources = awful.util.pread("xrdb -query")
 if not xresources:match(xresources_name) then
-    os.execute("urxvtd -q -o -f")
     os.execute("dex -a -e Awesome")
 end
 awful.util.spawn_with_shell("xrdb -merge <<< " .. "'" .. xresources_name .. ": true'")
