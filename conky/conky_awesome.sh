84c384857974e04964db764b6f02e2ab2f36dd19
paseu <paseu74@gmail.com>
2017-09-08 11:56:18 +0900

    divide config files according to type of machines

--- conky/conky_awesome.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/conky -c /home/paseu/.conky/Gotham_awesome -d
--- scripts/rr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while IFS='' read -r line || [[ -n "$line" ]]; do
-     mpv --sub-file="${line%.*}".vtt "$line"
-#    echo "Text read from file: $line"
-done < "$1"
-# read a file line by line
